fix scaling

src/run_attack.py
@@ -115,6 +115,8 @@ def main():
         
         optimizer = torch.optim.Adam([x_adv], lr=args.lr)
 
+        total_relevance_scaling = org_expl.sum() / target_expl.sum()
+
         arr_sample_x_adv = [
             x_adv.detach().cpu()
         ]
@@ -130,7 +132,7 @@ def main():
             loss_expl = F.mse_loss(
                 adv_expl / adv_expl.sum(),
                 # we make sure that the total relevance score is preserved.
-                target_expl / target_expl.sum() * org_expl.sum()
+                target_expl / target_expl.sum() * total_relevance_scaling
             )
             loss_output = F.mse_loss(adv_acc, org_acc.detach())
             total_loss = args.prefactors[0]*loss_expl + args.prefactors[1]*loss_output